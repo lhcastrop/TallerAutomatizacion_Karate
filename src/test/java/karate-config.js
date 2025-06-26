0bad6450b76cddd88e46fc2ad49baf23aa719cb8
Leonardo Castro <44737382+lhcastrop@users.noreply.github.com>
2025-06-26 18:27:06 -0500

    Update karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -7,7 +7,43 @@ function fn() {
     var server = '192.168.0.182:8080';
     if (karate.env == 'prod') {
         protocol = 'https';
-        server = 'parabank.parasoft.com';
+        server = 'function fn() {
+    karate.configure('connectTimeout', 5000);
+    karate.configure('readTimeout', 5000);
+    // karate.configure('abortSuiteOnFailure', true);
+
+    var protocol = 'http';
+    var server = '192.168.0.182:8080';
+    if (karate.env == 'prod') {
+        protocol = 'https';
+        server = 'function fn() {
+    karate.configure('connectTimeout', 5000);
+    karate.configure('readTimeout', 5000);
+    // karate.configure('abortSuiteOnFailure', true);
+
+    var protocol = 'http';
+    var server = '192.168.0.182:8080';
+    if (karate.env == 'prod') {
+        protocol = 'https';
+        server = 'https://thinking-tester-contact-list.herokuapp.com';
+    }
+
+    var config = {
+        baseUrl: protocol + '://' + server + '/parabank/services/bank'
+    };
+    config.faker = Java.type('com.github.javafaker.Faker');
+
+    return config;
+}';
+    }
+
+    var config = {
+        baseUrl: protocol + '://' + server + '/parabank/services/bank'
+    };
+    config.faker = Java.type('com.github.javafaker.Faker');
+
+    return config;
+}';
     }
 
     var config = {
