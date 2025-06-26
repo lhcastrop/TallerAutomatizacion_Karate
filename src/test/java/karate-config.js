d06e276a8c8dd5560db41e1eee85fffb2e04784c
Leonardo Castro <44737382+lhcastrop@users.noreply.github.com>
2025-06-26 18:44:11 -0500

    Update karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -7,47 +7,11 @@ function fn() {
     var server = '192.168.0.182:8080';
     if (karate.env == 'prod') {
         protocol = 'https';
-        server = 'function fn() {
-    karate.configure('connectTimeout', 5000);
-    karate.configure('readTimeout', 5000);
-    // karate.configure('abortSuiteOnFailure', true);
-
-    var protocol = 'http';
-    var server = '192.168.0.182:8080';
-    if (karate.env == 'prod') {
-        protocol = 'https';
-        server = 'function fn() {
-    karate.configure('connectTimeout', 5000);
-    karate.configure('readTimeout', 5000);
-    // karate.configure('abortSuiteOnFailure', true);
-
-    var protocol = 'http';
-    var server = '192.168.0.182:8080';
-    if (karate.env == 'prod') {
-        protocol = 'https';
-        server = 'https://thinking-tester-contact-list.herokuapp.com';
+        server = 'thinking-tester-contact-list.herokuapp.com';
     }
 
     var config = {
-        baseUrl: protocol + '://' + server + '/parabank/services/bank'
-    };
-    config.faker = Java.type('com.github.javafaker.Faker');
-
-    return config;
-}';
-    }
-
-    var config = {
-        baseUrl: protocol + '://' + server + '/parabank/services/bank'
-    };
-    config.faker = Java.type('com.github.javafaker.Faker');
-
-    return config;
-}';
-    }
-
-    var config = {
-        baseUrl: protocol + '://' + server + '/parabank/services/bank'
+        baseUrl: protocol + '://' + server,
     };
     config.faker = Java.type('com.github.javafaker.Faker');
 
